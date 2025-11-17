79d045c672cccc95e48621cb5870dbd055807c2f
leo0607y <reochimaru@gmail.com>
2025-11-17 20:50:03 +0900

    テスト追加

---
+++ distance_test.bash
@@ -0,0 +1,36 @@
+#!/bin/bash -xv
+#SPDX-FileCopyrightText: 2025 Reo Yamaguchi reo.yamaguchi0607@gmail.com
+#SPDX-License-Identifier: BSD-3-Clause
+
+ng (){
+	echo "❌ ${1}行目でテストが失敗しました"
+	res=1
+}
+
+res = 0
+
+#正常
+out = $(printf"Tokyo\nNY\n" | ./distance)
+
+[ "$?"=0 ] || ng "$LINENO"
+[ "$(echo "${out}" | grep -c "km")" = 1 ] || ng "$LINENO"
+[ "$(echo "${out}" | grep -c "km")" = 1 ] || ng "$LINENO"
+
+#エラー　実在しない
+Error_city="takiase"
+out=$(printf "Tokyo\n%s\n" "${CITY_INVALID}" | ./distance)
+
+[ "$?" = 1 ] || ng "$LINENO"
+[ "$(echo "${out}" | grep -c "Not found ${CITY_INVALID}")" = 1 ] || ng "$LINENO"
+
+#エラー　入力無し
+Nothingness_city=""
+out=$(printf "%s\nurayasu\n" "${CITY_EMPTY}" | ./distance)
+
+[ "$?" = 1 ] || ng "$LINENO"
+[ "$(echo "${out}" | grep -c "Not found ${CITY_EMPTY}")" = 1 ] || ng "$LINENO"
+
+#最終評価
+[ "${res}" = 0 ] && echo OK
+
+exit $res
